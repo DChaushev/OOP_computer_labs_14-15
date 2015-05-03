a6393bce1fe6fd75bc0c007648504e69c56cdd0c
Spas Kyuchukov <loloto.loloto@gmail.com>
2015-05-03 16:03:15 +0300

    Added Task02 (Stack) to Homework02

---
+++ Homework02/Stack.hpp
@@ -0,0 +1,21 @@
+// Interface for a template Stack
+// The required exceptions are in <stdexcept>, include it
+// You should also enable the exceptions in your compiler if necessary
+template<typename T>
+class Stack {
+public:
+    // Adds new element to the top of the stack
+    virtual void push(T x) = 0;
+    // Returns the top element AND removes it from the stack
+    // If the stack is empty use 'throw std::out_of_range("<human_friendly_message>");'
+    virtual T pop() = 0;
+    // Returns the top element but does NOT remove it from the stack
+    // If the stack is empty use 'throw std::out_of_range("<human_friendly_message>");'
+    virtual T top() = 0;
+    // Returns the current number of elements in the stack
+    virtual int getSize() = 0;
+    // Returns the current capacity of the underlying data storage (array)
+    virtual int getCapacity() = 0;
+    // Returns true if the stack has no elements and false otherwise
+    virtual bool isEmpty() = 0;
+};
